143ef01355e5139b2f2a03377ef831f553a7aa74
Melanie Castillo <mmcastillo1@uc.cl>
2024-10-16 19:37:03 -0300

    memory integration

--- src/osrms/main.c
+++ src/osrms/main.c
@@ -7,38 +7,38 @@ int main(int argc, char const *argv[])
     os_mount((char *)argv[1]);
 
     //resto de instrucciones
-    // printf("os_ls_processes()\n");
-    // printf("-----------------\n");
-    // os_ls_processes();
+    printf("os_ls_processes()\n");
+    printf("-----------------\n");
+    os_ls_processes();
 
-    // printf("os_exists(117, \"dino.jpg\")\n");
-    // printf("--------------------------\n");
-    // printf("Answer: %d\n\n", os_exists(117, "dino.jpg"));
+    printf("os_exists(117, \"dino.jpg\")\n");
+    printf("--------------------------\n");
+    printf("Answer: %d\n\n", os_exists(117, "dino.jpg"));
 
-    // printf("os_exists(91, \"hello.jpg\")\n");
-    // printf("--------------------------\n");
-    // printf("Answer: %d\n\n", os_exists(91, "hello.jpg"));
+    printf("os_exists(91, \"hello.jpg\")\n");
+    printf("--------------------------\n");
+    printf("Answer: %d\n\n", os_exists(91, "hello.jpg"));
 
-    // printf("os_ls_files(162)\n");
-    // printf("----------------\n");
-    // os_ls_files(162);
+    printf("os_ls_files(162)\n");
+    printf("----------------\n");
+    os_ls_files(162);
 
-    // printf("os_frame_bitmap()\n");
-    // printf("----------------\n");
-    // os_frame_bitmap();
+    printf("os_frame_bitmap()\n");
+    printf("----------------\n");
+    os_frame_bitmap();
 
-    // printf("os_tp_bitmap()\n");
-    // printf("--------------\n");
-    // os_tp_bitmap();
+    printf("os_tp_bitmap()\n");
+    printf("--------------\n");
+    os_tp_bitmap();
 
-    // printf("os_start_process(34, \"nuevo\")\n");
-    // printf("-----------------------------\n\n");
-    // os_start_process(43, "nuevo");
-    // printf("os_ls_processes()\n");
-    // printf("-----------------\n");
-    // os_ls_processes();
+    printf("os_start_process(34, \"nuevo\")\n");
+    printf("-----------------------------\n\n");
+    os_start_process(43, "nuevo");
+    printf("os_ls_processes()\n");
+    printf("-----------------\n");
+    os_ls_processes();
 
-    // os_finish_process(117);
+    os_finish_process(117);
 
     fclose(m_path);
     return 0;
